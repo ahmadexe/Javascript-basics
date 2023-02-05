f74ed20f8b3d91d7681a09e8266069898cf3d122
ahmadexe <muahmad710@gmail.com>
2023-02-06 04:58:58 +0500

    basic properties

---
+++ basics/script.js
@@ -0,0 +1,11 @@
+// 1. Ways to print in JavaScript - console API
+console.log("Hello world.");
+document.write("Hello world!");
+console.warn("This is a warning");
+console.error("This is an error");
+
+// for variables we can simply use var, let and const
+var und; // Variables by default on declaration are undefined
+console.log(und);
+
+// Symbols are also a primitive data type in JavaScript. Symbols are immutable, and unique. Symbols are often used to create unique object keys. 
