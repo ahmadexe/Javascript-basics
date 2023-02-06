46b450df9713bc4e5f2281670d9d4d764254047d
ahmadexe <muahmad710@gmail.com>
2023-02-06 05:37:49 +0500

    added array methods and docs for other properties

--- functions/script.js
+++ functions/script.js
@@ -1,25 +1,35 @@
-function avg(a, b) {
-  return (a + b) / 2;
-}
+// function avg(a, b) {
+//   return (a + b) / 2;
+// }
 
-console.log(avg(4, 11));
+// console.log(avg(4, 11));
 
-let a = [1,2,3,4,5];
+// let a = [1,2,3,4,5];
 
-for (let i = 0; i < a.length; i++) {
-  console.log(a[i]);
-}
+// for (let i = 0; i < a.length; i++) {
+//   console.log(a[i]);
+// }
 
-function iter(arr) {
-    arr.forEach(element => {
-        console.log(element);
-    });
-}
+// function iter(arr) {
+//     arr.forEach(element => {
+//         console.log(element);
+//     });
+// }
 
-iter(a);
+// iter(a);
 
-let i = 0;
-while (i < a.length) {
-  console.log(a[i]);
-  i++;
-}
+// let i = 0;
+// while (i < a.length) {
+//   console.log(a[i]);
+//   i++;
+// }
+
+let myArr = ["Fan", "Camera", 34, null, true];
+// Array Methods
+// console.log(myArr.length);
+// myArr.pop();
+// myArr.push("harry")
+// myArr.shift()
+// const newLen = myArr.unshift("Harry")
+// console.log(newLen);
+// console.log(myArr);
