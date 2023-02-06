50e75c7f764436ba072719b254e99174f97a767e
ahmadexe <muahmad710@gmail.com>
2023-02-06 05:37:15 +0500

    added loops and docs for other properties

--- functions/script.js
+++ functions/script.js
@@ -3,3 +3,23 @@ function avg(a, b) {
 }
 
 console.log(avg(4, 11));
+
+let a = [1,2,3,4,5];
+
+for (let i = 0; i < a.length; i++) {
+  console.log(a[i]);
+}
+
+function iter(arr) {
+    arr.forEach(element => {
+        console.log(element);
+    });
+}
+
+iter(a);
+
+let i = 0;
+while (i < a.length) {
+  console.log(a[i]);
+  i++;
+}
