5c801bc24f82fb4a0ec6b395ba6ef0ddcdc12415
ahmadexe <muahmad710@gmail.com>
2023-02-06 08:44:48 +0500

    string methods added

--- functions/script.js
+++ functions/script.js
@@ -36,3 +36,13 @@ let myArr = ["Fan", "Camera", 34, null, true];
 // myArr.sort(); sorts by compare function, by default, compare function is defined to sort the array by converting the elements into strings and comparing strings in Unicode code points order. We can pass our own compare function to sort the array in a different order.
 // myArr.sort(function(a, b) {return a-b}); // sorts the array in ascending order
 // myArr.sort(function(a, b) {return b-a}); // sorts the array in descending order
+
+// String Methods in JavaScript
+let str = "Harry is a good boy good good Harry";
+// console.log(myLovelyString.length)
+// console.log(myLovelyString.indexOf("good"))
+// console.log(myLovelyString.lastIndexOf("good"))
+// console.log(myLovelyString.slice(0, 4));
+d = str.replace("good", "Bad");
+console.log(d);
+
